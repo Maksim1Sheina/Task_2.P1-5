6f5c09d09bbf94f05a5b132393cbb88c3ba53d3b
Maksim1Sheina <hai_power@mail.ru>
2017-02-03 23:04:58 +0300

    Task_2.P1-5

---
+++ Task_2/Models/Task_2Context.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Task_2.Models
+{
+    public class Task_2Context : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public Task_2Context() : base("name=Task_2Context")
+        {
+            this.Database.Log = s => System.Diagnostics.Debug.WriteLine(s);
+        }
+
+        public System.Data.Entity.DbSet<Task_2.Models.Author> Authors { get; set; }
+
+        public System.Data.Entity.DbSet<Task_2.Models.Book> Books { get; set; }
+    }
+}
